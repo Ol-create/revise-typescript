37ba91ec511df95443f10cb1c89d797d345a28f9
Ol-create <oluola96@gmail.com>
2023-05-31 07:04:02 +0100

    declare any type and enum

--- dist/index.js
+++ dist/index.js
@@ -3,4 +3,5 @@ let sales = 123456789;
 let myName = 'Dr. Oluyemi';
 let isPresent = true;
 let score;
+let mySize = 2;
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,6 @@ let myName: string = 'Dr. Oluyemi'
 let isPresent: boolean = true;
 //score is type of any
 let score;
+
+const enum Size { small = 1, medium, large }
+let mySize: Size = Size.medium 
