7fa7d943c7e02b619d3af7f865145e3915931d4a
Ol-create <oluola96@gmail.com>
2023-09-23 16:24:59 +0100

    TypeScript of enum

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,9 @@
 "use strict";
-let person = ['OLuyemi', 29];
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 1] = "Small";
+    Size[Size["Medium"] = 2] = "Medium";
+    Size[Size["Large"] = 3] = "Large";
+})(Size || (Size = {}));
+console.log(Size.Medium);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-let person: [string, number] = ['OLuyemi', 29]
+// const small = 1;
+// const medium = 2;
+// const large = 3;
+
+enum Size { Small = 1, Medium, Large }
+
+console.log(Size.Medium)
