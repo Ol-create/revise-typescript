4176039122bf1fe16f69ff44290d3dbac16e795c
Ol-create <oluola96@gmail.com>
2023-09-24 02:03:03 +0100

    TypeScript of Alias

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
-let employee: { id: number, employeeName: string, retire: (date: Date) => void } = {
+type Employee = {
+    id: number, employeeName: string, retire: (date: Date) => void
+}
+
+let employee: Employee = {
     id: 1,
     employeeName: "Oluyemi",
     retire: (date: Date) => {
