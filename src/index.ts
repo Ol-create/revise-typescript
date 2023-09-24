3608c9281f65ab0d0eb208066c5191929023b150
Ol-create <oluola96@gmail.com>
2023-09-24 07:09:36 +0100

    TypeScript of type assertion

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,6 @@
-type Customer = {
-    birthday: Date
-}
+// Type assertion
+// let phone = document.getElementById('phone') as HTMLInputElement
 
-function getCustomer(id: number): Customer | null | undefined {
-    return id == 0 ? null : { birthday: new Date()}
-}
+let phone = <HTMLInputElement> document.getElementById('phone')
 
-let customer = getCustomer(0)
-   
-// if (customer !== null && customer !== undefined)
-//We can use the optional property access operator
-console.log(customer?.birthday?.getFullYear()) 
-    
-//We can do element optional access operator
-
-//customer?.[0]
-//optional call operator
-let log: any = null;
-log?.('a')
-
+phone.value
