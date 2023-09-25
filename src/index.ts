94f535450cda327002450072c0a60817b504bfd4
Ol-create <oluola96@gmail.com>
2023-09-25 14:24:46 +0100

    TypeScript of class private access modifier

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,32 @@
 class Account {
     readonly id: number;
     owner: string;
-    balance: number;
+    private _balance: number;
     nickname?: string
 
     constructor(id: number, owner: string, balance: number) {
         this.id = id;
         this.owner = owner;
-        this.balance = balance;
+        this._balance = balance;
     }
 
     deposite(amount: number): void {
         if (amount <= 0)
             throw new Error("Invalid amount")
-        this.balance += amount
+        this._balance += amount
+
+    }
+    getBalance(): number {
+        return this._balance
+    }
+
+    private calculateTax(): {
 
     }
 }
 
 let account = new Account(1, 'Oluyemi', 100)
+// let modifyBalance = (account.balance = 6);
 console.log(account)
-console.log(account.balance)
+console.log(account.getBalance())
 console.log('Is account an instance of Account: ', (account instanceof Account))
