9d13ced8a52721601e01163fabaeefadb6eb85b1
Ol-create <oluola96@gmail.com>
2023-09-25 15:15:35 +0100

    TypeScript of index signature property

--- dist/index.js
+++ dist/index.js
@@ -1,27 +1,7 @@
 "use strict";
-class Account {
-    constructor(id, owner, _balance) {
-        this.id = id;
-        this.owner = owner;
-        this._balance = _balance;
-    }
-    deposite(amount) {
-        if (amount <= 0)
-            throw new Error("Invalid amount");
-        this._balance += amount;
-    }
-    get balance() {
-        return this._balance;
-    }
-    set balance(value) {
-        if (value < 0)
-            throw new Error("Invalid balance");
-        this._balance = value;
-    }
+class seatAssignment {
 }
-let account = new Account(1, 'Oluyemi', 100);
-console.log(account);
-console.log(account.balance);
-console.log('New balance = ', account.balance = 300);
-console.log('Is account an instance of Account: ', (account instanceof Account));
+let seat = new seatAssignment;
+seat.A = 'Oluyemi';
+seat['B'] = 'Paul';
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,10 @@
-class Account {
-    nickname?: string
+class seatAssignment {
 
-    constructor(
-        public readonly id: number,
-        public owner: string,
-        private _balance: number) {
-    }
-
-    deposite(amount: number): void {
-        if (amount <= 0)
-            throw new Error("Invalid amount")
-        this._balance += amount
-
-    }
-    get balance(): number {
-        return this._balance
-    }
-
-    set balance(value: number) {
-        if (value < 0) 
-            throw new Error("Invalid balance")
-        this._balance = value
-    }
-
-    // private calculateTax(): {
-       
-    // }
+    //Index signature operators
+    [seat: string]: string
 }
 
-let account = new Account(1, 'Oluyemi', 100)
-// let modifyBalance = (account.balance = 6);
-console.log(account)
-console.log(account.balance)
-console.log('New balance = ', account.balance = 300)
-console.log('Is account an instance of Account: ', (account instanceof Account))
+let seat = new seatAssignment
+
+seat.A = 'Oluyemi';
+seat['B'] = 'Paul'
