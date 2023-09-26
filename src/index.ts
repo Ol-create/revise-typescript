1d1fe91e62971448bd955450a8ba22029aa1171a
Ol-create <oluola96@gmail.com>
2023-09-26 10:40:05 +0100

    TypeScript of generic type extending class

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,34 @@
-interface Result<T> {
-    data: T | null,
-    error: string | null,
-}
-
-function fetch<T>(url: string): Result<T>{
-  return {data: null, error: null}
-}
-
-interface User {
-    username: string
-}
-
 interface Product {
-    title: string
+    name: string,
+    price: number
 }
 
-let result = fetch<User>('url')
+class Store<T> {
+    protected _objects: T[] = [];
 
-result.data?.username
+    addObj(obj: T): void {
+        this._objects.push(obj)
+    }
+
+}
+
+//Pass on the generic type parameter
+class CompressibleStore<T> extends Store<T> {
+    compress() { };
+}
+
+let store = new CompressibleStore<Product>()
+
+//Restrict the generic type parameter
+class SearchableStore<T extends {name: string}> extends Store<T> {
+    find(name: string): T | undefined {
+        return this._objects.find(object => object.name === name)
+    }
+}
+
+//Fix the generic type parameter
+class ProductStore extends Store<Product> {
+    filterByCategory(category: string): Product[] {
+        return []
+    }
+}
